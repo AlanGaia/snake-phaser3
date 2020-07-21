6c4f0b72e6be158365468c80c854573e429f6548
Alan <devaliant94@gmail.com>
2020-07-21 18:12:08 -0300

    snake with 3 body movement

--- src/gameobjects/Snake.js
+++ src/gameobjects/Snake.js
@@ -1,10 +1,21 @@
 class Snake {
   constructor(scene) {
     this.scene = scene;
-    this.cuerpo = [this.scene.physics.add.image(10, 10, "cuerpo")];
+    this.cuerpo = [];
     this.dir = "derecha";
     this.timer = 0;
+    
+    
+    for (let i = 0; i < 3; i++) {
+      this.cuerpo.push(
+        this.scene.physics.add.image(10 + i * 10, 10, "cuerpo")
+          .setOrigin(0)
+        );
+      
+    }
+
   }
+
 
   changeMove(dir) {
     this.dir = dir;
@@ -12,6 +23,13 @@ class Snake {
 
   update(time) {
     if (time > this.timer) {
+
+      for(let i = this.cuerpo.length - 1; i > 0; i--){
+        this.cuerpo[i].x = this.cuerpo[i - 1].x; 
+        this.cuerpo[i].y = this.cuerpo[i - 1].y; 
+      }
+
+
       switch (this.dir) {
         case "derecha":
           this.cuerpo[0].x += 10;
